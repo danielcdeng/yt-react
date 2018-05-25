42e2d6c2af2dc218c383ac389a83359d4136a170
“Daniel <danielcdeng@gmail.com>
2018-05-25 12:14:03 -0700

    2018-05-23 bug fix.

--- src/components/common/AppTitle.js
+++ src/components/common/AppTitle.js
@@ -4,10 +4,10 @@ import wdates from '../../data/weeklyDates';
 
 const AppTitle = () => {
     return (
-      <div className="text-center">
+      <i className="text-center">
         <h1 className="colorDarkBlue"><i>{appConstants.ENUS_TITLE}</i></h1>
-        <h3>{wdates.fdate}&nbsp;-&nbsp;{wdates.tdate}</h3><br/>
-      </div>
+        <h3><i>{wdates.fdate}&nbsp;-&nbsp;{wdates.tdate}&nbsp;&nbsp;Weekly</i></h3>
+      </i>
     );
 };
 
